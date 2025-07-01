c3304d044af2196ebb797b0e1e027f340571d0da
00jup <jupark0369@gmail.com>
2025-07-01 10:39:27 +0900

    Feature: ListDivider Added

--- lib/presentations/theme/components/shapes/ListDivider.dart
+++ lib/presentations/theme/components/shapes/ListDivider.dart
@@ -1,1 +1,10 @@
+import 'package:flutter/material.dart';
 
+class ListDivider extends StatelessWidget {
+  const ListDivider({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(height: 1, color: Color.fromRGBO(128, 128, 128, 0.3));
+  }
+}
