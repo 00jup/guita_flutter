a9770148063b6e45b4ce656ad0b36e06830dc46f
00jup <jupark0369@gmail.com>
2025-07-01 10:39:45 +0900

    Feature: SongInfo Added

--- lib/features/lesson/entities/SongInfo.dart
+++ lib/features/lesson/entities/SongInfo.dart
@@ -1,1 +1,33 @@
+class SongInfo {
+  final String id;
+  final String level;
+  final String title;
+  final List<String> chords; // 임시로 String, 나중에 Chord enum으로 변경
+  final String fullSong;
 
+  const SongInfo({
+    required this.id,
+    required this.level,
+    required this.title,
+    required this.chords,
+    required this.fullSong,
+  });
+
+  String get truncatedTitle {
+    const maxCount = 20;
+    if (title.length > maxCount) {
+      return '${title.substring(0, maxCount)}…';
+    }
+    return title;
+  }
+
+  static List<SongInfo> get curriculum => [
+    const SongInfo(
+      id: "1",
+      level: "초급1",
+      title: "여행을 떠나요",
+      chords: ["A", "E", "B7"],
+      fullSong: "basic_1",
+    ),
+  ];
+}
